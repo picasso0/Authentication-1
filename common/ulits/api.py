61044828fabcfbd1678126cfb216903ab54d5929
picasso <hasanmt65@yahoo.com>
2022-02-27 15:57:34 +0330

    common : utils ( change directory )

--- common/ulits/api.py
+++ common/ulits/api.py
@@ -16,3 +16,18 @@ def response(code,data=[]):
         message,data,code,status="تابع ریسپانس با مشکل مواجه شده است",str(e),205,False
         
     return Response({"message":message,"data":data,"code":code,"status":status})
+
+
+def checkProject(request):
+    client_ip=get_client_ip(request=request)
+    return 0
+
+
+
+def get_client_ip(request):
+    x_forwarded_for = request.META.get('HTTP_X_FORWARDED_FOR')
+    if x_forwarded_for:
+        ip = x_forwarded_for.split(',')[0]
+    else:
+        ip = request.META.get('REMOTE_ADDR')
+    return ip
